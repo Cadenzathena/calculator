249c2e9fd8a495d0b3ba15f2712348ff1d6e3200
Cadenzathena <justinaddeh@gmail.com>
2026-05-18 03:40:39 +0100

    Implement calculator operator behaviour if an operator is the current input.

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const validDigits = '0123456789';
 const validOperators = '-+*/';
 
 
+
 // Ensures the input field doesn't accept values that aren't numbers or operators.
 // Replaces * with × for aesthetics.
 function inputSanitizer() {
@@ -15,14 +16,13 @@ function inputSanitizer() {
 }
 
 
+
 // handles various actions when an input is made with the input field in focus.
 function inputListener() {
     calcDisplayField.addEventListener("keydown", (e) => {
-        if (e.target.value === '0' &&
-            e.key.split('').some(item => validDigits.includes(item))) {
+        if (e.target.value === '0' && e.key.split('').some(item => validDigits.includes(item))) {
             e.target.value = e.target.value.replace('0', '');
         }
-
 
         // create a new array in omegaArr if it is empty!
         if (!omegaArr.length) {
@@ -30,16 +30,11 @@ function inputListener() {
         }
 
 
-
-        digitLogger();
-        operatorLogger();
-        backspaceHelper();
-
-
+        
         // Checks if the key pressed is a number and pushes the value to the last array in
         // omegaArr. Also check if the most recent array in omegaArr contains an
         // operator. True? generate a new array for only numbers.
-        function digitLogger() {
+        (function digitLogger() {
             if (e.key.split('').some(item => validDigits.includes(item))) {
                 if (omegaArr[omegaArr.length - 1].some(item => validOperators.includes(item))) {
                     omegaArr.push([]);
@@ -48,48 +43,69 @@ function inputListener() {
                 omegaArr[omegaArr.length - 1].push(e.key);
                 console.log(omegaArr);
             }
-        }
+        })();
+
 
 
         // Checks if the key pressed is an operator and pushes the value to the last array in
-        // omegaArr. Also check if the most recent array in omegaArr contains a
-        // number. True? generate a new array for only operators.
-        function operatorLogger() {
+        // omegaArr for a fresh array.
+        (function operatorLogger() {
             if (e.key.split('').some(item => validOperators.includes(item))) {
 
-                // if the user inputs an operator while only 0 is in the input field,
-                // add zero to an array quickly before adding the operator to its own array.
-                if (e.target.value === '0' &&
-                    !omegaArr[omegaArr.length - 1].length
-                ) {
-                    omegaArr[omegaArr.length - 1].push('0');
+                if (e.target.value === '0' && !omegaArr[omegaArr.length - 1].length) {  // if the user inputs an operator while only 0 is in the input field,
+                omegaArr[omegaArr.length - 1].push('0');                                // add zero to an array quickly before adding the operator to its own array.
                 }
 
                 if (omegaArr[omegaArr.length - 1].some(item => validDigits.includes(item))) {
                     omegaArr.push([]);
                 }
 
+                // the below code handles standard calculator operator behaviour if an operator is the current input.
+                if (omegaArr[omegaArr.length - 1].length == 2 && omegaArr[omegaArr.length - 1].some(item => validOperators.includes(item))) {
+                    omegaArr[omegaArr.length - 1].splice(0, omegaArr[omegaArr.length - 1].length);
+                    omegaArr[omegaArr.length - 1].push(e.key);
+                    console.log(omegaArr);
+                    return;
+                
+                } else if (omegaArr[omegaArr.length - 1].includes('+') || omegaArr[omegaArr.length - 1].includes('-')) {
+                
+                    omegaArr[omegaArr.length - 1].pop();
+                    omegaArr[omegaArr.length - 1].push(e.key);
+                    console.log(omegaArr);
+                    return;
+
+                } else if (e.key === '-' && (omegaArr[omegaArr.length - 1].includes('*') || omegaArr[omegaArr.length - 1].includes('/'))) {
+
+                    omegaArr[omegaArr.length - 1].push(e.key);
+                    console.log(omegaArr);
+                    return;
+
+                } else if (e.key !== '-' && (omegaArr[omegaArr.length - 1].includes('*') || omegaArr[omegaArr.length - 1].includes('/'))) {
+
+                    omegaArr[omegaArr.length - 1].pop();
+                    omegaArr[omegaArr.length - 1].push(e.key);
+                    console.log(omegaArr);
+                    return;
+                }
+                
                 omegaArr[omegaArr.length - 1].push(e.key);
                 console.log(omegaArr);
             }
-        }
+        })();
 
 
         // Deletes array items. Also helps clean up arrays in
         // omegaArr if they're empty after deletion
-        function backspaceHelper() {
+        (function backspaceHelper() {
             if (e.key === 'Backspace') {
                 omegaArr[omegaArr.length - 1].pop();
 
-            if (!omegaArr[omegaArr.length - 1].length) {
-                omegaArr.pop();
+                if (!omegaArr[omegaArr.length - 1].length) {
+                    omegaArr.pop();
+                }
+                console.log(omegaArr);
             }
-            console.log(omegaArr);
-            }
-        }
-
-
-
+        })();
     })
 }
 
